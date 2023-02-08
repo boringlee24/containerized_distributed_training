84928f874b2d1e291542d28b5a0a73275480b9a7
Baolin Li <baolin.li1994@gmail.com>
2023-02-08 00:30:12 -0500

    updated script to reflect latest benchmark

--- exec_benchmark_imagenet.sh
+++ exec_benchmark_imagenet.sh
@@ -4,5 +4,9 @@ set -x
 CONTAINER_ID=$1
 ARCH=$2
 NUM_GPU=$3
+GPU_TYPE=$4
 
-docker container exec -it $CONTAINER_ID python main.py -a $ARCH -b $((NUM_GPU*32)) imagenet
+docker container exec -it $CONTAINER_ID python main.py -a $ARCH \
+        -b $((NUM_GPU*64)) --benchmarking --gpu-type $GPU_TYPE \
+        --multiprocessing-distributed --world-size 1 --rank 0 \
+        --num-gpu $NUM_GPU imagenet
--- exec_benchmark_transformers.sh
+++ exec_benchmark_transformers.sh
@@ -4,12 +4,14 @@ set -x
 CONTAINER_ID=$1
 ARCH=$2
 NUM_GPU=$3
+GPU_TYPE=$4
 
 if (($NUM_GPU > 1)); then
         echo "${NUM_GPU}-GPU run"
         python3 set_num_gpu.py --num_gpu $NUM_GPU && sleep 1
         docker container exec -it $CONTAINER_ID accelerate launch run_qa_no_trainer.py --model_name_or_path $ARCH \
-                --dataset_name squad --max_seq_length 384 --doc_stride 128
+                --dataset_name squad --max_seq_length 384 --doc_stride 128 --benchmarking \
+                --gpu-type $GPU_TYPE --num-gpu $NUM_GPU
 else
         echo "Single-GPU run"
         docker container exec -it $CONTAINER_ID python3 run_qa_no_trainer.py --model_name_or_path $ARCH \
