4e7c919805a138bb9e5d1150c74c42375ff853eb
blueshark123 <44419907+blueshark123@users.noreply.github.com>
2023-02-12 17:54:19 -0500

    Update benchmarks.sh

--- benchmarks.sh
+++ benchmarks.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 set -x
 
-CONTAINER_ID=$1
-NUM_GPU=$2
-DEVICE_IDS=$3
-
-./exec_benchmark_imagenet.sh $CONTAINER_ID resnet50 $NUM_GPU nvidia $DEVICE_IDS \
-./exec_benchmark_imagenet.sh $CONTAINER_ID bit_b_16 $NUM_GPU nvidia $DEVICE_IDS
+./exec_benchmark_imagenet.sh $1 resnet50 $2 nvidia $3 \
+./exec_benchmark_imagenet.sh $1 bit_b_16 $2 nvidia $3
