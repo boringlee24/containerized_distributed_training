fa6fb6e8128cfaa7a1dee7ca937ebbe52f517a67
blueshark123 <44419907+blueshark123@users.noreply.github.com>
2023-02-12 17:27:30 -0500

    shell script to execute multiple benchmarks

---
+++ benchmarks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+CONTAINER_id=$1
+NUM_GPU=$2
+DEVICE_IDS=$3
+
+./exec_benchmark_imagenet.sh $CONTAINER_ID resnet50 $NUM_GPU nvidia $DEVICE_IDS \
+./exec_benchmark_imagenet.sh $CONTAINER_ID bit_b_16 $NUM_GPU nvidia $DEVICE_IDS
