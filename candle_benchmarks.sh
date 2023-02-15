330dc71eda2ac2a828e6015f8f2625fc5b4b110d
blueshark123 <44419907+blueshark123@users.noreply.github.com>
2023-02-14 19:16:15 -0500

    Create candle_benchmarks.sh

---
+++ candle_benchmarks.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+./exec_benchmark_candle.sh $1 Combo/combo $2 $3 $4 \
+./exec_benchmark_candle.sh $1 NT3/nt3 $2 $3 $4 \
+./exec_benchmark_candle.sh $1 P1B2/p1b2 $2 $3 $4 \
+./exec_benchmark_candle.sh $1 ST1/st1 $2 $3 $4 \
+./exec_benchmark_candle.sh $1 TC1/tc1 $2 $3 $4 
+
