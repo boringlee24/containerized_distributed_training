9d32cc8cf6275441c16e2c446594c8153ca930f9
Baolin Li <baolin.li1994@gmail.com>
2023-03-03 00:11:53 +0000

    update

--- imagenet_benchmarks.sh
+++ imagenet_benchmarks.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+set -x 
 
-./exec_benchmark_imagenet.sh $1 resnet50 $2 $3 $4 \
-./exec_benchmark_imagenet.sh $1 vit_b_16 $2 $3 $4 \
-./exec_benchmark_imagenet.sh $1 vgg19_bn $2 $3 $4 \
-./exec_benchmark_imagenet.sh $1 resnext50_32x4d $2 $3 $4 \
-./exec_benchmark_imagenet.sh $1 inception_v3 $2 $3 $4 \
+./exec_benchmark_imagenet.sh $1 resnet50 $2 $3 $4 && 
+sleep 30
+./exec_benchmark_imagenet.sh $1 vit_b_16 $2 $3 $4 && 
+sleep 30
+./exec_benchmark_imagenet.sh $1 vgg19_bn $2 $3 $4 &&
+sleep 30
+./exec_benchmark_imagenet.sh $1 resnext50_32x4d $2 $3 $4 &&
+sleep 30
+./exec_benchmark_imagenet.sh $1 inception_v3 $2 $3 $4 &&
+sleep 30
 ./exec_benchmark_imagenet.sh $1 shufflenet_v2_x1_5 $2 $3 $4 
